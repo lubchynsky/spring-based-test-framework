79d80dcd876e02225f46076ad51c09f94e0e9362
lubchynsky <lubchynsky.v@outlook.com>
2023-08-02 21:41:59 +0200

    Add selenide test

---
+++ src/main/java/com/lubchynsky/ui/SelenideHomePage.java
@@ -0,0 +1,30 @@
+package com.lubchynsky.ui;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import com.lubchynsky.configuration.spring.PropertiesReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+@Component
+public class SelenideHomePage {
+
+    private static final SelenideElement releasedSelenideVersion = $x("//div[contains(@class, 'news')]//*[contains(text(),'Released Selenide')]");
+
+    @Autowired
+    private PropertiesReader propertiesReader;
+
+    public SelenideHomePage openPage() {
+        Selenide.open(propertiesReader.getHostUrl());
+        return this;
+    }
+
+    public String getReleasedVersion() {
+        return releasedSelenideVersion.shouldBe(Condition.visible)
+                .getText();
+    }
+
+}
